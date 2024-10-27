c5eb45f8a8d79cf2de21a605b9265b872b620a7f
Kimberlynn01 <68210927+Kimberlynn01@users.noreply.github.com>
2024-10-27 18:18:10 +0000

    feat home

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -19,8 +19,20 @@
         @include('layouts.landingPage.navbar')
     </header>
 
-    <main>
-
+    <main class="py-20 px-5">
+        <section id="home">
+            <div class="container mx-auto my-20 text-center">
+                <h2 class="heading font-bold text-[46px] " style="font-family: 'Instrument Sans'; font-weight: bold;">
+                    Inspira: Simplify Your Portfolio Creation <br>& Unlock Potential
+                </h2>
+                <br>
+                <p class="w-[50%] text-[16px] text-[#828282] mx-auto text-center" style="font-family: 'Instrument Sans'; font-weight: 400; text-align:center;">Inspira memudahkan pembuatan portofolio profesional dengan cepat dan praktis. Dilengkapi template menarik dan fitur fleksibel, Inspira membantu menampilkan karya terbaik Anda secara elegan dan mudah diakses. Bangun portofolio impian dan tunjukkan potensi Anda dengan percaya diri bersama <br>Inspira!</p>
+                <div class="button flex justify-center mt-[30px]">
+                    <button class="text-[#FFFFFF] bg-[#1C1C1C] py-3 mx-2 text-[16px] px-5 rounded" style="font-family: 'Instrument Sans'; font-weight: bold;">Let's Join</button>
+                    <button class="text-[#1C1C1C] bg-[#CCCCCC] py-3 mx-2 text-[16px] px-5 rounded" style="font-family: 'Instrument Sans'; font-weight: bold;">Learn Now!</button>
+                </div>
+            </div>
+        </section>
     </main>
 
 </body>
